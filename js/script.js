3d5424cb8dd69a3fc9c80b85787ed23e48f68bba
CthRobo <cthrobo@gmail.com>
2023-03-13 14:39:29 +0100

    gjorde så att gridbox-elementen förstoras oc tar upp hela gridboxen när de klickas

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-const gridItems = document.querySelectorAll('#projekt-grid > div');
+const gridItems = document.querySelectorAll('.grid-element');
 
 gridItems.forEach(item => {
 
   let widthScale = (item.offsetWidth + 20) / item.offsetWidth;
 
-  item.addEventListener('mouseover', () => {
-    item.style.transform = `scale(${widthScale})`;
-    item.style.zIndex = '1';
-  });
   
+
+  item.addEventListener('mouseover', () => {
+    if (!item.classList.contains("expanded")) {
+      item.style.transform = `scale(${widthScale})`;
+      item.style.zIndex = '1';
+    }
+  });
+    
   item.addEventListener('mouseout', () => {
     item.style.transform = 'scale(1)';
     item.style.zIndex = '0';
   });
+  
+
+  item.addEventListener("click", toggleExpand);
 });
+
+function toggleExpand(event) {
+  const item = event.target;
+  const items = document.querySelectorAll(".grid-element");
+  items.forEach((item) => {
+    if (item !== event.target) {
+      item.classList.toggle("hidden");
+    }
+  });
+  item.classList.toggle("expanded");
+  item.style.transform = 'scale(1)';
+}
 
 const navbar = document.querySelector('.navbar');
 const burgerMenu = navbar.querySelector('.burger-menu');
